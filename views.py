cc43815deb34f90fbc965cdb8ef5da9103d4a8b9
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2022-03-29 01:50:19 +0600

    these all are of profile1 app
    profile1app

--- views.py
+++ views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render
 
 
-
-def home(request):
-    return render(request, 'Home/index.html')
+def userProfile(request,pk):
+    return render(request, 'profile1/user_profile.html')
